da3a4f3f162a8c16dc52ab3d4d8b5941a5bc0bad
david.haver <d.n.h.s.b.j@gmail.com>
2024-01-25 14:55:33 +0200

    x->lease_manager

--- client/lease_manager/lease_manager.c
+++ client/lease_manager/lease_manager.c
@@ -1,123 +1,161 @@
+#include <stdlib.h>
 #include "lease_manager.h"
 
-int createLease(struct leaseManager *lm) {
-    int ret = 0;
-    int screen, err;
-    xcb_generic_error_t *xerr;
-    xcb_connection_t *connection;
+#ifdef DEBUG
+#include <stdio.h>
+#include <string.h>
+#endif
+
+static void failureCleanUp
+(xcb_connection_t *connection, xcb_randr_get_screen_resources_reply_t *resourcesReply);
+
+int createLease(lease_manager_t *lm) {
+    int screen = 0;
+    xcb_randr_get_screen_resources_reply_t *resourcesReply = NULL;
 
     //connecting to the X server
-    connection = xcb_connect(NULL, &screen);
-    if (!connection) {
-        printf("failed to connect to X server\n");
-        ret = -1;
-        goto failed_x_con;
-    }
+    xcb_connection_t *connection = xcb_connect(NULL, &screen);
 
     //checking connection error
-    err = xcb_connection_has_error(connection);
-    if (err) {
+    int err = xcb_connection_has_error(connection);
+    if(err) 
+    {
+        #ifdef DEBUG
         printf("connection has error: %s\n", strerror(err));
-        ret = -2;
-        goto failed_x_con;
+        #endif
+        failureCleanUp(connection, resourcesReply);
+        return LEASE_MANAGER_CONNECTION_FAILED;
     }
     
     //getting root window
-    const xcb_setup_t *setup = xcb_get_setup(connection);
-    xcb_screen_iterator_t screenItr = xcb_setup_roots_iterator(setup);
-    int screenIndex = 0;
-    while (screenItr.rem) {
-        if (screenIndex == screen) {
-            break;
-        }
-        xcb_screen_next(&screenItr);
-        screenIndex++;
+    xcb_screen_iterator_t screenItr = xcb_setup_roots_iterator(xcb_get_setup(connection));
+
+    for(; screenItr.rem && screen; xcb_screen_next(&screenItr), --screen)
+    {
+        //empty
     }
     xcb_window_t root = screenItr.data->root;
 
-    //getting screen resources
-    xcb_randr_get_screen_resources_cookie_t resourcesCookie = xcb_randr_get_screen_resources(connection, root);
-    xcb_randr_get_screen_resources_reply_t *resourcesReply = xcb_randr_get_screen_resources_reply(connection, resourcesCookie, &xerr);
-    if (!resourcesReply) {
+    //getting screen resources for lease
+    xcb_generic_error_t *xerr = NULL;
+
+    xcb_randr_get_screen_resources_cookie_t resourcesCookie = 
+    xcb_randr_get_screen_resources(connection, root);
+
+    resourcesReply = 
+    xcb_randr_get_screen_resources_reply(connection, resourcesCookie, &xerr);
+    if(!resourcesReply) 
+    {
+        #ifdef DEBUG
         printf("failed to get screen resources: %s\n", strerror(xerr->error_code));
-        ret = -3;
-        goto failed_x_con;
+        #endif
+        failureCleanUp(connection, resourcesReply);
+        return LEASE_MANAGER_FAILED_TO_GET_REPLY;
     }
 
-    xcb_randr_output_t *outputs = xcb_randr_get_screen_resources_outputs(resourcesReply);
+    xcb_randr_output_t *outputsArray = xcb_randr_get_screen_resources_outputs(resourcesReply);
     xcb_randr_output_t output = 0;
-    for (int o=0; output==0 && o<resourcesReply->num_outputs; o++) {
-        xcb_randr_get_output_info_cookie_t outputCookie = xcb_randr_get_output_info(connection, outputs[o], resourcesReply->config_timestamp);
-        xcb_randr_get_output_info_reply_t *outputReply = xcb_randr_get_output_info_reply(connection, outputCookie, &xerr);
-        if (!outputReply) {
+    for(int i = 0; 0 == output && i < resourcesReply->num_outputs; ++i) 
+    {
+        xcb_randr_get_output_info_cookie_t outputCookie = 
+        xcb_randr_get_output_info(connection, outputsArray[i], resourcesReply->config_timestamp);
+
+        xcb_randr_get_output_info_reply_t *outputReply = 
+        xcb_randr_get_output_info_reply(connection, outputCookie, &xerr);
+        if (!outputReply) 
+        {
+            #ifdef DEBUG
             printf("failed to get output from X server: %s\n", strerror(xerr->error_code));
-            ret = -4;
-            goto failed_output;
+            #endif
+            failureCleanUp(connection, resourcesReply);
+            return LEASE_MANAGER_FAILED_TO_GET_OUTPUT;
         }
-        if (outputReply->connection == XCB_RANDR_CONNECTION_CONNECTED) {
-            output = outputs[o];
+        if (outputReply->connection == XCB_RANDR_CONNECTION_CONNECTED) 
+        {
+            output = outputsArray[i];
         }
         free(outputReply);
     }
-    if (!output) {
+
+    if(!output) 
+    {
+        #ifdef DEBUG
         printf("output not found\n");
-        ret = -5;
-        goto failed_output;
+        #endif
+        failureCleanUp(connection, resourcesReply);
+        return LEASE_MANAGER_FAILED_TO_GET_OUTPUT;
     }
 
-    xcb_randr_crtc_t *crtcs = xcb_randr_get_screen_resources_crtcs(resourcesReply);
+    xcb_randr_crtc_t *crtcsArray = xcb_randr_get_screen_resources_crtcs(resourcesReply);
     xcb_randr_crtc_t crtc = 0;
-    for (int c=0; crtc==0 && c<resourcesReply->num_crtcs; c++) {
-        xcb_randr_get_crtc_info_cookie_t crtcCookie = xcb_randr_get_crtc_info(connection, crtcs[c], resourcesReply->config_timestamp);
-        xcb_randr_get_crtc_info_reply_t *crtcReply = xcb_randr_get_crtc_info_reply(connection, crtcCookie, &xerr);
-        if (!crtcReply) {
+    for(int i=0;  0 == crtc && i<resourcesReply->num_crtcs; ++i) 
+    {
+        xcb_randr_get_crtc_info_cookie_t crtcCookie = 
+        xcb_randr_get_crtc_info(connection, crtcsArray[i], resourcesReply->config_timestamp);
+
+        xcb_randr_get_crtc_info_reply_t *crtcReply = 
+        xcb_randr_get_crtc_info_reply(connection, crtcCookie, &xerr);
+        if(!crtcReply) 
+        {
+            #ifdef DEBUG
             printf("failed to get crtc from X server: %s\n", strerror(xerr->error_code));
-            ret = -6;
-            goto failed_crtc;
+            #endif
+            failureCleanUp(connection, resourcesReply);
+            return LEASE_MANAGER_FAILED_TO_GET_CRTC;
         }
-        if (crtcReply->mode != 0) {
-            crtc = crtcs[c];
+        if (crtcReply->mode != 0) 
+        {
+            crtc = crtcsArray[i];
         }
         free(crtcReply);
     }
-    if (!crtc) {
+
+    if(!crtc) 
+    {
+        #ifdef DEBUG
         printf("crtc not found\n");
-        ret = -7;
-        goto failed_crtc;
+        #endif
+        failureCleanUp(connection, resourcesReply);
+        return LEASE_MANAGER_FAILED_TO_GET_CRTC;
     }
 
     free(resourcesReply);
     
     xcb_randr_lease_t lease = xcb_generate_id(connection);
-    xcb_randr_create_lease_cookie_t leaseCookie = xcb_randr_create_lease(connection, root, lease, 1, 1, &crtc, &output);
-    xcb_randr_create_lease_reply_t *leaseReply = xcb_randr_create_lease_reply(connection, leaseCookie, &xerr);
-    if (!leaseReply) {
+    xcb_randr_create_lease_cookie_t leaseCookie = 
+    xcb_randr_create_lease(connection, root, lease, 1, 1, &crtc, &output);
+
+    xcb_randr_create_lease_reply_t *leaseReply = 
+    xcb_randr_create_lease_reply(connection, leaseCookie, &xerr);
+    if(!leaseReply) 
+    {
+        #ifdef DEBUG
         printf("failed to create lease from X server: %s\n", strerror(xerr->error_code));
-        ret = -8;
-        goto failed_lease;
+        #endif
+        failureCleanUp(connection, resourcesReply);
+        return LEASE_MANAGER_FAILED_TO_CREATE_LEASE;
     }
+
+    lm->connection = connection;
+    lm->leaseReply = leaseReply;
 
     //although this function docs say return value needs to be freed
     //trying to free the pointer will result in invalid pointer
     //and no leaks all be found by valgrind
     int *leaseFD = xcb_randr_create_lease_reply_fds(connection, leaseReply);
-    int fd = leaseFD[0];
 
-    lm->connection = connection;
-    lm->leaseReply = leaseReply;
-
-    return fd;
-
-failed_lease:
-failed_crtc:
-failed_output:
-    free(resourcesReply);
-failed_x_con:
-    xcb_disconnect(connection);
-    return ret;
+    return leaseFD[0];
 }
 
-void revokeLease(struct leaseManager *lm) {
+void revokeLease(lease_manager_t *lm) 
+{
     free(lm->leaseReply);
     xcb_disconnect(lm->connection);
+}
+
+void failureCleanUp
+(xcb_connection_t *connection, xcb_randr_get_screen_resources_reply_t *resourcesReply)
+{
+    xcb_disconnect(connection);
+    free(resourcesReply);
 }
--- client/lease_manager/lease_manager.h
+++ client/lease_manager/lease_manager.h
@@ -1,22 +1,22 @@
 #ifndef _LEASE_MANAGER_H_
 #define _LEASE_MANAGER_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <errno.h>
-#include <string.h>
-
-/* #include <xcb/xcb.h> */
 #include <xcb/randr.h>
 
-struct leaseManager {
+#define LEASE_MANAGER_CONNECTION_FAILED (-1)
+#define LEASE_MANAGER_FAILED_TO_GET_REPLY (-2)
+#define LEASE_MANAGER_FAILED_TO_GET_OUTPUT (-3)
+#define LEASE_MANAGER_FAILED_TO_GET_CRTC (-4)
+#define LEASE_MANAGER_FAILED_TO_CREATE_LEASE (-5)
+
+typedef struct lease_manager 
+{
     xcb_connection_t *connection;
     xcb_randr_create_lease_reply_t *leaseReply;
-};
+}lease_manager_t;
 
-int createLease(struct leaseManager *lm);
+int createLease(lease_manager_t *lm);
 
-void revokeLease(struct leaseManager *lm);
+void revokeLease(lease_manager_t *lm);
 
 #endif /*_LEASE_MANAGER_H_*/
